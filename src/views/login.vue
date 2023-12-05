0b08ac5aa9a4c0b8b5ba7bd4dfd4b70ae2c723ae
Leslie <416317444@qq.com>
2023-12-05 11:17:38 +0800

    完成登录页基本结构

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,11 +1,78 @@
 <template>
-
+  <div class="login">
+    <div class="login__container">
+      <el-form
+        ref="ruleFormRef"
+        :model="ruleForm"
+        :rules="rules"
+        status-icon
+        label-width="100px"
+      >
+        <el-form-item label="账号" prop="username">
+          <el-input v-model="ruleForm.username" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm(ruleFormRef)"> 登录 </el-button>
+          <el-button @click="resetForm(ruleFormRef)">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+import type { FormInstance, FormRules } from 'element-plus'
+interface RuleForm {
+  username: string
+  password: string
+}
 
+const ruleFormRef = ref<FormInstance>()
+const ruleForm = reactive<RuleForm>({
+  username: '',
+  password: ''
+})
+
+const rules = reactive<FormRules<RuleForm>>({
+  username: [
+    { required: true, message: '请输入您的用户名', trigger: 'blur' },
+    { min: 5, max: 20, message: '用户名长度必须大于 5', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入您的密码', trigger: 'blur' },
+    { min: 5, max: 20, message: '密码长度必须大于 5', trigger: 'blur' }
+  ]
+})
+
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 
+  .login__container {
+    width: 500px;
+  }
+}
 </style>
