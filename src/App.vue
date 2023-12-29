3cc4b2e6fc54c32f3f3f7e8cbc4b0969b4de4e1d
Leslie <416317444@qq.com>
2023-12-29 21:19:20 +0800

    chore:修改格式

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <RouterView />
+  <router-view></router-view>
 </template>
 
-<style lang="scss"></style>
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
