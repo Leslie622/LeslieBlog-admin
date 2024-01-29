6c56f7159bfbbe8c760f152096867ac5b7cae291
Leslie <416317444@qq.com>
2024-01-29 14:30:13 +0800

    chore:修改nprogress配置

--- src/utils/nprogress.ts
+++ src/utils/nprogress.ts
@@ -9,7 +9,7 @@ NProgress.configure({
   showSpinner: false, // 是否显示加载ico
   trickleSpeed: 200, // 自动递增间隔
   minimum: 0.3, // 更改启动时使用的最小百分比
-  parent: '#app', //指定进度条的父容器
+  parent: '.app', //指定进度条的父容器
 })
 
 export default NProgress
