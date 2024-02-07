c817d5cab885172c7835a64a6a8736f7136e01c8
Leslie <416317444@qq.com>
2024-02-07 21:12:58 +0800

    feat:网站 icon&title&logo

--- src/layout/components/SideBar/index.vue
+++ src/layout/components/SideBar/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="sidebar">
-    <div class="sidebar__logo">Logo</div>
+    <div class="sidebar__logo">
+      <img src="@/assets/image/logo.png" alt="" :class="[commonStore.isMenuCollapse ? 'small' : 'big']" />
+    </div>
     <Menu></Menu>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCommonStore } from '@/stores/modules/common'
+const commonStore = useCommonStore()
+</script>
 
 <style lang="scss" scoped>
 .sidebar {
@@ -17,10 +22,20 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    width: 100%;
     height: 50px;
     font-size: 20px;
     color: red;
     background-image: $backImgDark;
+    img {
+      transition: all 0.5s;
+      &.big {
+        height: 45%;
+      }
+      &.small {
+        height: 30%;
+      }
+    }
   }
 }
 </style>
