80cab5a6877b7b8f89a7e36e4feae452e05b223e
Leslie <416317444@qq.com>
2024-02-17 21:12:32 +0800

    feat:欢迎页样式翻新

--- src/views/welcome/UserRegister/index.vue
+++ src/views/welcome/UserRegister/index.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="register">
     <div class="register__container">
+      <div class="hint">
+        <p>注册 ~</p>
+      </div>
       <el-form ref="registerFormRef" :model="registerForm" :rules="rules" status-icon>
-        <el-form-item label="账号" prop="account">
-          <el-input v-model="registerForm.account" />
+        <el-form-item prop="account">
+          <span class="input input--kaede">
+            <input class="input__field input__field--kaede" type="text" id="account" v-model="registerForm.account" />
+            <label class="input__label input__label--kaede" for="account">
+              <span class="input__label-content input__label-content--kaede">
+                <Icon icon="material-symbols:account-circle-outline" width="20px"></Icon>
+                <span>账号</span>
+              </span>
+            </label>
+          </span>
         </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="registerForm.password" />
+        <el-form-item prop="password">
+          <span class="input input--kaede">
+            <input class="input__field input__field--kaede" type="text" id="password" v-model="registerForm.password" />
+            <label class="input__label input__label--kaede" for="password">
+              <span class="input__label-content input__label-content--kaede">
+                <Icon icon="material-symbols:key-outline-rounded" width="20px"></Icon>
+                <span>密码</span>
+              </span>
+            </label>
+          </span>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm(registerFormRef)" :loading="isLoading">
-            注册
-          </el-button>
-          <el-button @click="resetForm(registerFormRef)">重置</el-button>
+          <div class="button">
+            <a  @click="submitForm(registerFormRef)" :loading="isLoading"> 注册 </a>
+          </div>
+          <div class="button">
+            <a  @click="resetForm(registerFormRef)" :loading="isLoading"> Reset </a>
+          </div>
         </el-form-item>
       </el-form>
     </div>
@@ -57,7 +78,7 @@ const submitForm = async (formEl: FormInstance | undefined) => {
       try {
         //请求注册接口：该逻辑在store/user中处理
         await userStore.register(registerForm)
-        ElMessage.success("注册成功，开始登录吧！")
+        ElMessage.success('注册成功，开始登录吧！')
         emit('change-tab', 'login')
         isLoading.value = false
       } catch {
@@ -79,7 +100,152 @@ const resetForm = (formEl: FormInstance | undefined) => {
 </script>
 
 <style lang="scss" scoped>
+.button {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  margin-right: 20px;
+
+  a {
+    text-decoration: none;
+    color: #000;
+    margin: auto;
+    width: 70px;
+    display: inline-block;
+    line-height: 25px;
+    font-size: 15px;
+    font-weight: bold;
+    letter-spacing: 2px;
+    background-color: #fff;
+    border: 2px solid #000;
+    box-shadow:
+      1px 1px 0,
+      2px 2px 0;
+    position: relative;
+    cursor: pointer;
+  }
+
+  a:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    z-index: -1;
+    background-color: #fff;
+    transition: all 0.3s;
+  }
+
+  a:hover {
+    z-index: 0;
+    background-color: transparent;
+  }
+
+  a:hover:after {
+    background-color: #ee3f4d;
+  }
+
+  a:active {
+    top: 2px;
+    left: 2px;
+    box-shadow: 0 0 0 0;
+  }
+}
+
 .register__container {
-  width: 400px;
+  width: 100%;
+
+  :deep(.el-form-item__content) {
+    justify-content: center;
+  }
+
+  :deep(.el-form-item) {
+    margin-bottom: 30px;
+  }
+
+  .hint {
+    font-size: 30px;
+    padding-left: 10px;
+    p {
+      margin: 0;
+      margin-bottom: 30px;
+    }
+  }
+}
+
+.input {
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  border-radius: 5px;
+
+  .input__field {
+    position: relative;
+    border: none;
+  }
+
+  .input__field:focus {
+    outline: none;
+  }
+
+  .input__label {
+    padding: 0 10px;
+  }
+
+  .input__label-content {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    line-height: 35px;
+    color: #857f7f;
+  }
+}
+
+.input--kaede {
+  overflow: hidden;
+  background-image: $backInputLight;
+
+  .input__field--kaede {
+    position: absolute;
+    top: 0;
+    right: 100%;
+    width: 60%;
+    height: 100%;
+    transition: transform 0.5s;
+    transition-timing-function: cubic-bezier(0.2, 1, 0.3, 1);
+  }
+
+  .input__label--kaede {
+    display: block;
+    cursor: text;
+    transform-origin: 0% 50%;
+    transition: transform 0.5s;
+    transition-timing-function: cubic-bezier(0.2, 1, 0.3, 1);
+  }
+
+  .input__label-content--kaede {
+    padding: 5px 0;
+  }
+
+  .input__field--kaede:focus,
+  .input--filled .input__field--kaede {
+    transform: translate3d(100%, 0, 0);
+    transition-delay: 0.06s;
+  }
+
+  .input__field--kaede:focus + .input__label--kaede,
+  .input--filled .input__label--kaede {
+    transform: translate3d(60%, 0, 0);
+    pointer-events: none;
+  }
+
+  @media screen and (max-width: 34em) {
+    .input__field--kaede:focus + .input__label--kaede,
+    .input--filled .input__label--kaede {
+      transform: translate3d(65%, 0, 0) scale3d(0.65, 0.65, 1);
+      pointer-events: none;
+    }
+  }
 }
 </style>
--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,45 +1,128 @@
 <template>
-  <div class="tabs">
-    <el-tabs type="border-card" v-model="activeName" class="tabs__content">
-      <el-tab-pane label="登录" name="login">
-        <user-login></user-login>
-      </el-tab-pane>
-      <el-tab-pane label="注册" name="register">
-        <user-register @change-tab="activeName = 'login'"></user-register>
-      </el-tab-pane>
-    </el-tabs>
+  <div class="welcome__wrapper">
+    <div class="welcome__content">
+      <div class="illustration"></div>
+      <div class="entrance">
+        <div class="logo">
+          <img src="../../assets/image/logoFont.png" alt="" />
+        </div>
+        <div class="entrance__content">
+          <Transition name="fade" mode="out-in">
+            <component :is="activeComponent"></component>
+          </Transition>
+        </div>
+        <div class="action">
+          <div class="register__btn" v-if="activeComponent === UserRegister" @click="activeComponent = UserLogin">
+            <span>去登录</span>
+            <Icon icon="material-symbols:keyboard-double-arrow-right-rounded" width="18px" class="icon"></Icon>
+          </div>
+          <div class="register__btn" v-if="activeComponent === UserLogin" @click="activeComponent = UserRegister">
+            <span>去注册</span>
+            <Icon icon="material-symbols:keyboard-double-arrow-right-rounded" width="18px" class="icon"></Icon>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const activeName = ref('login')
 import UserLogin from './UserLogin/index.vue'
 import UserRegister from './UserRegister/index.vue'
+
+const activeComponent: any = shallowRef(UserLogin)
 </script>
 
 <style lang="scss" scoped>
-.tabs {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background-image: url("../../assets/image/brillant.png");
-  
+.welcome__wrapper {
+  display: grid;
+  place-items: center;
+  height: 100%;
+  background-image: $backImgEcailles;
+  user-select: none;
 
-  .tabs__content {
-    width: 500px;
-    height: 300px;
-    border-radius: 5px;
+  .welcome__content {
+    display: flex;
+    width: 750px;
+    height: 463px;
+    border-radius: 10px;
     overflow: hidden;
-   
-    :deep(.el-tabs__content) {
-      padding: 0;
-      box-sizing: content-box;
-      height: calc(100% - 40px);
+    background-color: white;
+    box-shadow:
+      rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+
+    .entrance {
+      z-index: 0;
       display: flex;
-      align-items: center;
-      justify-content: center;
+      flex-direction: column;
+      flex: 1;
+      padding: 5px 10px;
+      background-image: $backImgLight;
+
+      .logo {
+        display: flex;
+        align-items: center;
+        justify-content: end;
+        height: 20px;
+
+        img {
+          height: 80%;
+          opacity: 0.15;
+        }
+      }
+
+      .entrance__content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: 70px 20px 0px;
+        div {
+          width: 100%;
+        }
+      }
+
+      .action {
+        height: 30px;
+        display: flex;
+        justify-content: end;
+
+        .register__btn {
+          display: flex;
+          align-items: center;
+          transition: all 0.3s;
+          cursor: pointer;
+          color: #857f7f;
+
+          span {
+            position: relative;
+            bottom: -1px;
+          }
+          &:hover {
+            color: black;
+          }
+        }
+      }
+    }
+
+    .illustration {
+      position: relative;
+      z-index: 100;
+      flex: 1;
+      background-image: url('@/assets/image/illustration.jpg');
+      background-size: cover;
+      box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px;
     }
   }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
